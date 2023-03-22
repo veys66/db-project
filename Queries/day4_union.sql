af270691c1089507f148fc48ca3b8c6c2e353cbe
veys66 <veysdoganer@gmail.com>
2023-03-22 21:51:40 +0300

    sql

--- Queries/day4_union.sql
+++ Queries/day4_union.sql
@@ -1,3 +1,18 @@
 select * from testers
-union all
+minus
 select * from developers;
+
+select names from testers
+union
+select names from developers;
+
+select names from developers
+intersect
+select names from testers;
+
+--how to find duplicate names in employees table
+
+select FIRST_NAME, count(*)
+from EMPLOYEES
+group by FIRST_NAME
+having count(*) > 1;
